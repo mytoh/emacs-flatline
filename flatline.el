a7d2b78b6b47aebe04831ccbc00fc20d447c51dc
mytoh <mytoh@gmail.com>
2013-12-27 14:30:42 +0900

    flatline: fix face names

--- flatline.el
+++ flatline.el
@@ -17,19 +17,19 @@
 (cl-defun flatline:column ()
   `(:eval (propertize "%02c" 'face
                       (if (>= (current-column) 80)
-                          'flatline:80col-face
-                        'flatline:column-face))))
+                          'flatline:face-80col
+                        'flatline:face-column))))
 
 (cl-defun flatline:line ()
-  `(:eval (propertize "%02l" 'face 'flatline:line-face)))
+  `(:eval (propertize "%02l" 'face 'flatline:face-line)))
 
 (cl-defun flatline:modified ()
   `(:eval
     (cond (buffer-read-only
-           (propertize " RO " 'face 'flatline:read-only-face))
+           (propertize " RO " 'face 'flatline:face-read-only))
           ((buffer-modified-p)
-           (propertize " ** " 'face 'flatline:modified-face))
-          (t (propertize " -- " 'face 'flatline:modified-face)))))
+           (propertize " ** " 'face 'flatline:face-modified))
+          (t (propertize " -- " 'face 'flatline:face-modified)))))
 
 (cl-defun flatline:mule-info ()
   `((current-input-method
@@ -38,7 +38,7 @@
 
 (cl-defun flatline:mnemonic ()
   `(:eval
-    (propertize (format-mode-line "%z") 'face 'flatline:mnemonic-face)))
+    (propertize (format-mode-line "%z") 'face 'flatline:face-mnemonic)))
 
 (setq eol-mnemonic-undecided ":")
 (setq eol-mnemonic-unix "unix")
@@ -55,46 +55,46 @@
       (setq desc
             (propertize
              mnemonic
-             'face 'flatline:eol-desc-face))
+             'face 'flatline:face-eol-desc))
       (push (cons eol (cons mnemonic desc)) mode-line-eol-desc-cache)
       desc)))
 
 (cl-defun flatline:client ()
   `(:eval
     (propertize (if (frame-parameter nil 'client) "@" "/")
-                'face 'flatline:client-face)))
+                'face 'flatline:face-client)))
 
 (cl-defun flatline:buffer ()
   `(:eval
     (propertize "%b"
-                'face 'flatline:buffer-face)))
+                'face 'flatline:face-buffer)))
 
 (cl-defun flatline:major-mode ()
   `(:eval
-    (propertize mode-name 'face 'flatline:major-mode-face)
-    (propertize mode-line-process 'face 'flatline:major-mode-face)
-    (propertize "%n" 'face 'flatline:major-mode-face)))
+    (propertize mode-name 'face 'flatline:face-major-mode)
+    (propertize mode-line-process 'face 'flatline:face-major-mode)
+    (propertize "%n" 'face 'flatline:face-major-mode)))
 
 (cl-defun flatline:minor-mode ()
   `(:eval
-    (propertize (format-mode-line minor-mode-alist) 'face 'flatline:minor-mode-face)))
+    (propertize (format-mode-line minor-mode-alist) 'face 'flatline:face-minor-mode)))
 
 (cl-defun flatline:warning ()
-  `(:eval (propertize "%e" 'face 'flatline:warning-face)))
+  `(:eval (propertize "%e" 'face 'flatline:face-warning)))
 
 (cl-defun flatline:misc-info ()
   `(:propertize
     mode-line-misc-info
-    face flatline:misc-info-face))
+    face flatline:face-misc-info))
 
 (cl-defun flatline:nyan-mode ()
   `(:eval (when nyan-mode (list (nyan-create)))))
 
 (cl-defun flatline:space (space)
-  `(:eval (propertize ,space 'face 'flatline:space-face)))
+  `(:eval (propertize ,space 'face 'flatline:face-space)))
 
 (cl-defun flatline:vc-mode ()
-  `(:eval (propertize vc-mode 'face 'flatline:vc-mode-face)))
+  `(:eval (propertize vc-mode 'face 'flatline:face-vc-mode)))
 
 (cl-defun flatline:shorten-path (path)
   (cl-letf ((npath (cl-remove-if #'(lambda (s) (string= s ""))
@@ -111,8 +111,7 @@
      (t path))))
 
 (cl-defun flatline:buffer-directory ()
-  `(:eval (propertize (flatline:shorten-path default-directory) 'face 'flatline:buffer-directory-face)))
-
+  `(:eval (propertize (flatline:shorten-path default-directory) 'face 'flatline:face-buffer-directory)))
 
 
 ;; faces
@@ -121,8 +120,7 @@
   "Faces used in the mode line."
   :group 'flatline)
 
-
-(defface flatline:normal-face
+(defface flatline:face-normal
   '((t (:background "gray20" :foreground "gray60" :inverse-video nil :box nil)))
   "flatline base face"
   :group 'flatline-face)
@@ -130,101 +128,101 @@
 (set-face-attribute 'mode-line nil :foreground "gray60" :background "gray20" :inverse-video nil :box nil)
 
 ;; inactive face
-(defface flatline:inactive-face
+(defface flatline:face-inactive
   '((t (:foreground "white" :background "black")))
   "flatline inactive face"
   :group 'flatline-face)
 (set-face-attribute 'mode-line-inactive nil
-                    :inherit 'flatline:inactive-face)
+                    :inherit 'flatline:face-inactive)
 
 ;; warning
-(defface flatline:warning-face
+(defface flatline:face-warning
   '((t (:background "gray20" :foreground "red")))
   "flatline warning face"
   :group 'flatline-face)
 
 ;; modified
-(defface flatline:modified-face
+(defface flatline:face-modified
   '((t ( :background "gray20" :foreground "#c82829")))
   "flatline modified face"
   :group 'flatline-face)
-(defface flatline:read-only-face
+(defface flatline:face-read-only
   '((t ( :background "gray20" :foreground "#4271ae" :box '(:line-width 2 :color "#4271ae"))))
   "flatline read only face"
   :group 'flatline-face)
 
 ;; column
-(defface flatline:column-face
-  '((t (:inherit 'flatline:normal-face)))
+(defface flatline:face-column
+  '((t (:inherit 'flatline:face-normal)))
   "flatline column face"
   :group 'flatline-face)
-(defface flatline:80col-face
+(defface flatline:face-80col
   '((t ( :background "gray20" :foreground "red")))
   "flatline 80col face"
   :group 'flatline-face)
 
 ;; line
-(defface flatline:line-face
+(defface flatline:face-line
   '((t (:foreground "gray60" :background "gray20")))
   "flatline line face"
   :group 'flatline-face)
 
 ;; mule info
-(defface flatline:mnemonic-face
+(defface flatline:face-mnemonic
   '((t (:foreground "gray60" :background "gray20")))
   "flatline mnemonic face"
   :group 'flatline-face)
 
 ;; eol desc
-(defface flatline:eol-desc-face
+(defface flatline:face-eol-desc
   '((t (  :background "gray20" :foreground "#93ef5a")))
   "flatline eol description face"
   :group 'flatline-face)
 
 ;; client
-(defface flatline:client-face
-  '((t (:inherit 'flatline:normal-face)))
+(defface flatline:face-client
+  '((t (:inherit 'flatline:face-normal)))
   "flatline client face"
   :group 'flatline-face)
 
 ;; buffer name
-(defface flatline:buffer-face
+(defface flatline:face-buffer
   '((t ( :background "#3a3a4f" :foreground "#a7bc7a")))
   "flatline buffer face"
   :group 'flatline-face)
 
 ;; buffer directory
-(defface flatline:buffer-directory-face
+(defface flatline:face-buffer-directory
   '((t (:foreground "#65cba2" :background "gray20")))
   "flatline buffer directory face"
   :group 'flatline-face)
 
 ;; mode name
-(defface flatline:major-mode-face
+(defface flatline:face-major-mode
   '((t (  :background "gray20" :foreground "#bb3bbb")))
   "flatline mode name face"
   :group 'flatline-face)
 
 ;; minor mode
-(defface flatline:minor-mode-face
+(defface flatline:face-minor-mode
   '((t (  :background "gray20" :foreground "#6b719e")))
   "flatline minor made face"
   :group 'flatline-face)
 
 ;; misc info
-(defface flatline:misc-info-face
+(defface flatline:face-misc-info
   '((t ( :background "gray20" :foreground "#c55f3e")))
   "flatline misc info face"
   :group 'flatline-face)
 
 ;; vc-mode info
-(defface flatline:vc-mode-face
+(defface flatline:face-vc-mode
   '((t (:background "gray20" :foreground "#3e9cbb")))
   "flatline vc mode face"
-  :group 'flatline-fale)
+  :group 'flatline-face)
 
 ;; space
-(defface flatline:space-face
+(defface flatline:face-space
   '((t (:foreground "gray60" :background "gray20")))
   "flatline space face"
   :group 'flatline-face)
@@ -252,22 +250,22 @@
    ;; modified
    (flatline:modified)
    ;; column
-   '(:eval (propertize "(" 'face 'flatline:normal-face))
+   '(:eval (propertize "(" 'face 'flatline:face-normal))
    (flatline:column)
-   '(:eval (propertize "," 'face 'flatline:normal-face))
+   '(:eval (propertize "," 'face 'flatline:face-normal))
    (flatline:line)
-   '(:eval (propertize ")" 'face 'flatline:normal-face))
+   '(:eval (propertize ")" 'face 'flatline:face-normal))
    (flatline:space "  ")
    ;; vc mode
    (flatline:vc-mode)
    (flatline:space "  ")
 
-   '(:eval (propertize "(" 'face 'flatline:normal-face))
+   '(:eval (propertize "(" 'face 'flatline:face-normal))
    ;; major mode
    (flatline:major-mode)
    ;; minor mode
    (flatline:minor-mode)
-   '(:eval (propertize ")" 'face 'flatline:normal-face))
+   '(:eval (propertize ")" 'face 'flatline:face-normal))
 
    (flatline:space "  ")
    ;; misc inf
