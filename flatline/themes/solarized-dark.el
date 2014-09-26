877c53be3601454cb23a813ee224455bb7aa21b7
mytoh <mytoh@gmail.com>
2014-09-27 05:54:34 +0900

    * flatline/themes/solarized-dark.el: add evil colours

--- flatline/themes/solarized-dark.el
+++ flatline/themes/solarized-dark.el
@@ -2,34 +2,58 @@
 
 
 (defface flatline-solarized-dark-left
-  '((t (:foreground "#fdf6e3"
-                    :background "#268bd2"
-                    :box nil)))
+    '((t (:foreground "#fdf6e3"
+          :background "#268bd2"
+          :box nil)))
   "face for left")
 
 (defface flatline-solarized-dark-left-sub
-  '((t (:foreground "#fdf6e3"
-                    :background "#586e75"
-                    :box nil)))
+    '((t (:foreground "#fdf6e3"
+          :background "#586e75"
+          :box nil)))
   "face for left")
 
 (defface flatline-solarized-dark-right
-  '((t (:foreground "#073642"
-                    :background  "#839496"
-                    :box nil)))
+    '((t (:foreground "#073642"
+          :background  "#839496"
+          :box nil)))
   "face for sub")
 
 (defface flatline-solarized-dark-right-sub
-  '((t (:foreground "#93a1a1"
-                    :background  "#586e75"
-                    :box nil)))
+    '((t (:foreground "#93a1a1"
+          :background  "#586e75"
+          :box nil)))
   "face for sub")
 
 (defface flatline-solarized-dark-middle
-  '((t (:foreground "#93a1a1"
-                    :background  "#073642"
-                    :box nil)))
+    '((t (:foreground "#93a1a1"
+          :background  "#073642"
+          :box nil)))
   "face for left sub sub")
+
+(defface flatline-solarized-dark-evil-normal
+    '((t (:foreground  "white"
+          :background "#2aa198"
+          :box nil)))
+  "face for evil normal")
+
+(defface flatline-solarized-dark-evil-insert
+    '((t (:foreground  "white"
+          :background  "#cb4b16"
+          :box nil)))
+  "face for evil insert")
+
+(defface flatline-solarized-dark-evil-visual
+    '((t (:foreground  "white"
+          :background  "#d33682"
+          :box nil)))
+  "face for evil visual")
+
+(defface flatline-solarized-dark-evil-operator
+    '((t (:foreground  "white"
+          :background "#6c71c4"
+          :box nil)))
+  "face for evil operator")
 
 (provide 'flatline-theme-solarized-dark)
 
